c4231776079596a3c9062c7f8da0516353b36080
Miguel-AngelGarcia <mgar574@wgu.edu>
2022-11-02 17:00:27 -0400

    Testing extension

--- food/views.py
+++ food/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'hello.html', {'name' : 'Teddy'})
+
+def item(request):
+    return HttpResponse('This will become an item view')
