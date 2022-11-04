e19d4a1b7544eaed61f858777d8e1ce76aadda55
Miguel-AngelGarcia <mgar574@wgu.edu>
2022-11-04 15:40:32 -0400

    Hid the secret key

--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+#added to not publish secret key
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "hehe"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
@@ -39,7 +45,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "playground",
     "debug_toolbar",
+    "django.contrib.sessions",
+    #added the above bc we were getting an error
+    #we then ran python5 manage.py migrate bc needed'''
+
 ]
+
+
+#SITE_ID = 1
 
 INTERNAL_IPS = [
     # ...
